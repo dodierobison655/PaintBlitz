e7cd2fbc6efc6b48ec1abc7a3b5c464f5b72d9f4
Deral Robison <dodierobison655@gmail.com>
2016-12-06 14:11:56 -0600

    Updated circles

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 var canvas = document.getElementById('myCanvas');
-if(canvas.getContext){
-    var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
+addCirc(350,350,10,0,Math.PI*2,"white");
+
+addCirc(350,100,25,0,Math.PI*2,"blue";
+addCirc(350,225,25,0,Math.PI*2,"purple");
+addCirc(220,225,25,0,Math.PI*2,"red");
+addCirc(480,225,25,0,Math.PI*2,"yellow");
+addCirc(100,350,25,0,Math.PI*2,"green");
+addCirc(220,350,25,0,Math.PI*2,"blue");
+addCirc(480,350,25,0,Math.PI*2,"pink");
+addCirc(600,350,25,0,Math.PI*2,"orange");
+addCirc(220,480,25,0,Math.PI*2,"brown");
+addCirc(350,480,25,0,Math.PI*2,"purple");
+addCirc(480,480,25,0,Math.PI*2,"green");
+addCirc(350,600,25,0,Math.PI*2,"black");
+
+addCirc(50,50,25,0,Math.PI*2,"white");
+addCirc(650,50,25,0,Math.PI*2,"white");
+addCirc(50,650,25,0,Math.PI*2,"white");
+addCirc(650,650,25,0,Math.PI*2,"white");
+function addCirc(x,y,r,s,c,fill){
     ctx.beginPath();
-    ctx.arc(350,350,10,0,Math.PI*2); // Outer circle
+    ctx.arc(x,y,r,s,c);
     ctx.stroke();
-    ctx.fillStyle="white";
+    ctx.fillStyle = fill;
     ctx.fill();
-  }
+}
+
